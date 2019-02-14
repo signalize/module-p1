a2df06c4a973dcf08e1a615a7c5d60521f1f1561
Maikel ten Voorde <info@matevo.nl>
2019-02-14 13:14:55 +0100

    Refactor

--- src/Device.php
+++ src/Device.php
@@ -4,7 +4,12 @@ namespace Signalize\ModuleP1;
 
 class Device extends \Signalize\Hardware\Serial
 {
-    public function process($chuck, $buffer): Package
+    /**
+     * @param string $chuck
+     * @param string $buffer
+     * @return \Signalize\Hardware\Package
+     */
+    public function process(string $chuck, string $buffer): \Signalize\Hardware\Package
     {
         if (substr($buffer, 0, 1) === '/') {
             if (substr($chuck, 0, 1) === '!') {
