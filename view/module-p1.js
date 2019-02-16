b6f41213e89b0cae9db1bc5ba083dac83448231c
Maikel ten Voorde <info@matevo.nl>
2019-02-16 21:52:30 +0100

    Initial Commit

--- view/module-p1.js
+++ view/module-p1.js
@@ -1,10 +1,31 @@
 import {Widget} from "/js/widget.js";
 
 customElements.define('module-p1', class extends Widget {
-    channel = 'service-module-p1';
+    static service = 'service-module-p1';
 
-    render() {
-        let name = 'World';
-        return `Hello ${name}`;
+    static render(data) {
+        console.log(data);
+
+        return Widget.parse`
+        <h1>Electricity</h1>
+
+        <h2>Result</h2>
+        ${data ? `
+        	<div>${data.electricity.result.meter1}</div>
+			<div>${data.electricity.result.meter2}</div>
+			<h2>Usage</h2>
+	        <div>${data.electricity.usage.meter1}</div>
+	        <div>${data.electricity.usage.meter2}</div>
+        ` : `
+        	Loading...
+        `}
+        
+        <h1>Gas</h1>
+		${data ? `
+        	<div>Amount: ${data.gas}</div>
+        ` : `
+        	Loading...
+        `}
+        `;
     }
 });
