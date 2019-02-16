0a9ab491346a43da67676d6886c7aabba426bca3
Maikel ten Voorde <info@matevo.nl>
2019-02-16 20:12:09 +0100

    Initial Commit

---
+++ view/module-p1.js
@@ -0,0 +1,12 @@
+import {Widget} from "/js/widget.js";
+
+export class ModuleP1 {
+    constructor() {
+        customElements.define('module-p1', class extends Widget {
+            render() {
+                let name = 'World';
+                return `Hello ${name}`;
+            }
+        });
+    }
+}
--- view/widget.js
+++
@@ -1,6 +0,0 @@
-export class Widget {
-    constructor() {
-        customElements.define('widget-module-p1', class extends HTMLElement {
-        });
-    }
-}
