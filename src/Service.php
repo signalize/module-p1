a3aa9a38f5f1c8d69681905a05b45ee0c95edb32
Maikel ten Voorde <info@matevo.nl>
2019-02-19 09:59:11 +0100

    Initial Commit

--- src/Service.php
+++ src/Service.php
@@ -13,7 +13,8 @@ class Service extends Base
         $device = new Device("/dev/ttyUSB0");
         $device->subscribe(function (Package $package) {
             Database::store('statistics', $package);
-            $this->send($package);
+
+            $this->send(new Package($package));
         });
     }
 
@@ -34,17 +35,23 @@ class Service extends Base
 
     private function fetch(Package $package)
     {
-        $from = $package->offsetExists('from') ? $package->offsetExists('from') : strtotime(date("d-m-Y 00:00:00"));
-        $untill = $package->offsetExists('from') ? $package->offsetExists('from') : strtotime(date("d-m-Y 00:00:00") . " +1day");
+        $from = $package->offsetExists('from') ? $package->offsetGet('from') : strtotime(date("d-m-Y 00:00:00"));
+        $untill = $package->offsetExists('untill') ? $package->offsetGet('untill') : strtotime(date("d-m-Y 23:59:59"));
 
         $data = Database::collection('statistics')->find(function ($row) use ($from, $untill) {
-            return (
-                ($row->datetime > $from) &&
-                ($row->datetime < $untill)
-            );
+            return ($row->datetime > $from && $row->datetime < $untill);
         });
 
-        return new Package($data->toArray());
+
+        $plot = [];
+        foreach ($data->toArray() as $row) {
+            $date = date("ymdH", $row->datetime);
+            if (!isset($plot[$date])) {
+                $plot[$date] = 0;
+            }
+            $plot[$date]++;
+        }
+        return new Package($plot);
     }
 
 }
