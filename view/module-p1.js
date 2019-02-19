8e3905d2d724c0d367a23dcc1db89f7dd1fa2703
Maikel ten Voorde <info@matevo.nl>
2019-02-19 09:59:36 +0100

    Initial Commit

--- view/module-p1.js
+++ view/module-p1.js
@@ -9,20 +9,20 @@ customElements.define('module-p1', class extends Widget {
         return Widget.parse`
         <h1>Electricity</h1>
 
-        <h2>Result</h2>
+        <h2>Usage</h2>
         ${data ? `
-        	<div>${data.energy.usage.low}</div>
-			<div>${data.energy.usage.high}</div>
-			<h2>Usage</h2>
-	        <div>${data.energy.result.low}</div>
-	        <div>${data.energy.result.high}</div>
+        	<div>${data['energy.usage.low']}</div>
+			<div>${data['energy.usage.high']}</div>
+			<h2>Result</h2>
+	        <div>${data['energy.result.low']}</div>
+	        <div>${data['energy.result.high']}</div>
         ` : `
         	Loading...
         `}
         
         <h1>Gas</h1>
 		${data ? `
-        	<div>Amount: ${data.gas.usage.total}</div>
+        	<div>Amount: ${data['gas.usage.total']}</div>
         ` : `
         	Loading...
         `}
